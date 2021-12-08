5c148e8b2b7dfc1334c3499a3c5712704f8be81e
AdemilsonB <80437316+AdemilsonB@users.noreply.github.com>
2021-12-07 22:50:53 -0300

    Update FolhaPagamento.cs

--- API/Models/FolhaPagamento.cs
+++ API/Models/FolhaPagamento.cs
@@ -7,13 +7,14 @@ namespace API.Models
 {
     public class FolhaPagamento
     {
-        public FolhaPagamento() => CriadoEm = DateTime.Now;
-        public int FolhaPagamentoId { get; set; }
+         public FolhaPagamento() => CriadoEm = DateTime.Now;
+        public int Id { get; set; }
         public int FuncionarioId { get; set; }
-        public Funcionario funcionario { get; set; }
+        public Funcionario Funcionario { get; set; }
         public int QntdHoras { get; set; }
         public int ValorHora { get; set; }
         public int Mes { get; set; }
+        public int Ano { get; set; }
         public DateTime CriadoEm { get; set; }
     }
 }
